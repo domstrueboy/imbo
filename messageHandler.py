7567e6a0b2f3c0b8265cdde853ad250f153c9d50
domstrueboy <domstrueboy@yandex.ru>
2017-06-15 11:25:10 +0500

    v.0.0.2

--- messageHandler.py
+++ messageHandler.py
@@ -4,8 +4,13 @@ from base import voc
 
 def check_answer(body, item):
     data = body['body'].strip().lower()
-    while data.endswith('?') or data.endswith('.') or data.endswith('!'):
-        data = data[0 : len(data)-1]
+
+    if data.endswith('?'):
+        sign = '?'
+    else:
+        sign = ''
+
+    data = data.replace('?', '').replace('!', '').replace('.', '').replace('`', '').replace("'", '').replace(',', '')
 
     flag = False
     for i in item['en'] :
@@ -13,23 +18,24 @@ def check_answer(body, item):
             flag = True
             break
 
-    if item['type'] == '+' or item['type'] == '-':
-        sign = '.'
-    elif item['type'] == '?':
-        sign = '?'
-
-
+    sticker = False
     if flag :
+        sticker = random.choice([1,37])
         message = 'Right!'
     else :
+        sticker = random.choice([35])
         message = '"' + body['body'] + '" is wrong!\n"' + item['en'][0].capitalize() + sign + '" is the right answer'
 
-    return message
+    return message, sticker
 
 
 
-def get_question():
-    message = random.choice(voc)
+def get_question(previous_question):
+    while True:
+        message = random.choice(voc)
+        if message != previous_question:
+            break
+
     return message
 
 
@@ -37,15 +43,17 @@ def get_question():
 def create_answer(data, token):
    user_id = data['user_id']
 
-   if data['body'] == 'go':
-       message = get_question()
+   if data['body'].strip().lower() == 'go':
+       create_answer.voc_item = ''
+       message = get_question(create_answer.voc_item)
        create_answer.voc_item = message
        vkapi.send_message(user_id, token, message['ru'] + ' (' + ', '.join(message['tips']) + ')')
 
    else:
-       message = check_answer(data, create_answer.voc_item)
+       message, sticker = check_answer(data, create_answer.voc_item)
        vkapi.send_message(user_id, token, message)
+       #vkapi.send_message(user_id, token, message, sticker)
 
-       message = get_question()
+       message = get_question(create_answer.voc_item)
        create_answer.voc_item = message
        vkapi.send_message(user_id, token, message['ru'] + ' (' + ', '.join(message['tips']) + ')')
--- vkapi.py
+++ vkapi.py
@@ -3,5 +3,8 @@ import vk
 session = vk.Session()
 api = vk.API(session)
 
-def send_message(user_id, token, message):
-    api.messages.send(access_token=token, user_id=str(user_id), message=message)
+def send_message(user_id, token, message, sticker=False):
+    if sticker:
+        api.messages.send(access_token=token, user_id=str(user_id), message=message, sticker_id=sticker)
+    else:
+        api.messages.send(access_token=token, user_id=str(user_id), message=message)
