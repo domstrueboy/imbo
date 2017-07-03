3dcd423099f02c18a67dfbae9f07267fb6e980da
domstrueboy <domstrueboy@yandex.ru>
2017-07-03 23:20:08 +0500

    для переноса на рабочий

--- static/main.js
+++ static/main.js
@@ -5,17 +5,35 @@ var app = new Vue({
   data:
   {
   	base: base,
-  	ru: this.base[ getRandomInt(0, this.base.length-1) ].ru,
+    num: getRandomInt(0, this.base.length-1),
+  	ru: this.base[ num ].ru,
     en: '',
     pronounses: ['I', 'He', 'She', 'It', 'You', 'We', 'They'],
     auxiliary: ['am', 'is', 'are', 'was', 'were', 'will', 'do', 'does', 'did', 'not', 'no']
+  },
+
+  computed: {
+
   },
 
   methods: {
   	inputWord: function(word)
   	{
   		app.en += word + ' ';
-  	}
+  	},
+
+    checkPhrase: function(){
+      if( app.en === his.base[ num ].en )
+      {
+        alert('Right!');
+        app.num = getRandomInt(0, app.base.length-1);
+        ru: app.base[ num ].ru
+      }
+      else
+      {
+        alert('Wrong!');
+      }
+    }
   }
 })
 
