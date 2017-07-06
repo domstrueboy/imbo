11b5e1a0a483614a590a31f205d5b2c645f08fd2
bpv <bpv>
2017-07-06 17:59:40 +0500

    v.0.0.7

--- static/main.js
+++ static/main.js
@@ -1,5 +1,6 @@
 'use strict'
-var app = new Vue({
+var app = new Vue(
+{
   el: '#app',
 
   data:
@@ -8,26 +9,75 @@ var app = new Vue({
     phrase: this.base[ getRandomInt(0, this.base.length-1) ],
     input: '',
     ok: null,
-    pronounses: [
+    pronouns: [
     			  ['i'],
     			  ['he', 'she', 'it'],
     			  ['you', 'we', 'they']
     			],
     auxiliary: [
     			  ['am', 'is', 'are', 'was', 'were', 'will', 'be'],
+    			  ['isnt', 'arent', 'wasnt', 'werent', 'wont' ],
     			  ['do', 'does', 'did'],
+    			  ['dont', 'doesnt', 'didnt'],
     			  ['not', 'no']
     			]
   },
 
-  computed: {
+  computed:
+  {
+  	verbs: function()
+  	{
+  		var verbs = [];
+  		for(let i in this.phrase.en)
+  		{
+  			verbs = verbs.concat(
+  									this.phrase.en[i].replace(',', ' ')
+  				  				  			 	 	 .replace(/  +/g, ' ')
+  				  				  			 	 	 .replace('`', '')
+    						 				 		 .replace("'", '')
+  				  				  			 		 .split(' ')
+  				  				)
+  		}
 
+  		let buf = verbs;
+
+  		for(let i in verbs)
+  		{
+  			this.pronouns.forEach( function(str)
+  			{
+  				if( str.includes(verbs[i]) )
+	  			{
+	  				buf[i] = null; 
+	  			}
+  			});
+
+  			this.auxiliary.forEach( function(str)
+  			{
+  				if( str.includes(verbs[i]) )
+	  			{
+	  				buf[i] = null; 
+	  			}
+  			});
+  		}
+
+  		console.log(buf);
+
+  		verbs = buf.filter(function(element)
+		{
+			return element;
+		});
+
+  		console.log(verbs);
+  		return verbs;
+  	}
   },
 
-  methods: {
+  methods:
+  {
   	inputWord: function(word)
   	{
   		app.input += word + ' ';
+  		app.input = app.input[0].toUpperCase() + app.input.substring(1);
   	},
 
   	updatePhrase: function()
@@ -37,8 +87,8 @@ var app = new Vue({
         app.ok = null;
   	},
 
-    checkPhrase: function(){
-
+    checkPhrase: function()
+    {
     	var input = app.input.trim()
     						 .toLowerCase()
     						 .replace('?', '')
@@ -46,7 +96,8 @@ var app = new Vue({
     						 .replace('.', '')
     						 .replace('`', '')
     						 .replace("'", '')
-    						 .replace(',', '');
+    						 .replace(',', ' ')
+    						 .replace(/  +/g, ' '); // it replaces several spaces to one
 
     	var flag = false;
 
@@ -69,12 +120,23 @@ var app = new Vue({
 	        app.ok = false;
 	        setTimeout(app.updatePhrase, 5000);
 	    }
+
+	    inp.focus();
+    },
+
+    cleanPhrase: function()
+    {
+    	app.input = '';
+    	inp.focus();
     }
   }
-})
+}
+)
 
 
 
-function getRandomInt(min, max) {
+function getRandomInt(min, max)
+{
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
